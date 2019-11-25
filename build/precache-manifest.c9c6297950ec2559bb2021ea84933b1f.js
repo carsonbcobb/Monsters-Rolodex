40efa554e22aa8fdb81b2a8400f9f8017859445c
carsonbcobb <I3carebears>
2019-11-25 14:10:20 -0800

    adding files for github pages

---
+++ build/precache-manifest.c9c6297950ec2559bb2021ea84933b1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01fc7f05de5b0e02a21da4124085f2f",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "b509c9d4a9526c66afb5",
+    "url": "/Monsters-Rolodex/static/css/main.5a7e1797.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/Monsters-Rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "b509c9d4a9526c66afb5",
+    "url": "/Monsters-Rolodex/static/js/main.fce0ff55.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
